66969b0b55b8b8c14ab9e47535c4df64be906a6c
sd0ric4 <1286518974@qq.com>
2025-03-17 21:09:13 +0800

    更新一下

--- app/routes/api/$.tsx
+++ app/routes/api/$.tsx
@@ -1,14 +1,22 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from 'react-router';
 
+// 扩展 RequestInit 类型
+interface ExtendedRequestInit extends RequestInit {
+  duplex?: 'half';
+}
+
 export async function loader({ request }: LoaderFunctionArgs) {
   const apiUrl = process.env.PROXY_TARGET || 'http://192.168.0.118:8080';
   const url = new URL(request.url);
   const targetUrl = `${apiUrl}${url.pathname.replace('/api', '')}${url.search}`;
 
-  return fetch(targetUrl, {
+  const options: ExtendedRequestInit = {
     headers: request.headers,
     method: request.method,
-  });
+    duplex: 'half',
+  };
+
+  return fetch(targetUrl, options);
 }
 
 export async function action({ request }: ActionFunctionArgs) {
@@ -16,9 +24,12 @@ export async function action({ request }: ActionFunctionArgs) {
   const url = new URL(request.url);
   const targetUrl = `${apiUrl}${url.pathname.replace('/api', '')}${url.search}`;
 
-  return fetch(targetUrl, {
+  const options: ExtendedRequestInit = {
     method: request.method,
     headers: request.headers,
     body: request.body,
-  });
+    duplex: 'half',
+  };
+
+  return fetch(targetUrl, options);
 }
